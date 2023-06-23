cc25d3ee2c403c3e6d12489f890546918719e416
wendy <mizukilee@gmail.com>
2023-06-22 22:49:27 -0400

    added user/password + google signin

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,20 +1,52 @@
-// import { auth, googleProvider } from "../config/firebase";
-// import {
-//   createUserWithEmailAndPassword,
-//   signInWithPopup,
-//   signOut,
-// } from "firebase/auth";
-// import { useState } from "react";
+import { auth, googleProvider } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth"; // these are methods from firebase
+import { useState } from "react";
+
+// with firebase the password has to be longer than 6 
+
+
+
 
 export const Auth = () => { 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+const signIn = async () => {
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err); // console.error is used specifically to print error messages 
+  }
+};
+
+const logout = async () => { 
+  try { 
+    await signOut(auth)
+  } catch (err) { 
+    console.error(err) 
+  }
+}
+  
+// const signInWithGoogle = async () => { 
+//   try { // handle errors 
+//   await signInWithPopup(auth, googleProvider) // 
+//   } catch (err) { 
+//   console.error(err); 
+//   } 
+// }; 
+  
+  
   return ( 
     <div>
-      <input placeholder='Email...'> 
-        Log In 
-      </input>
-      <input placeholder='Password...'> 
-        Password 
-      </input>
+      <input placeholder='Email...' onChange = {e => setEmail(e.target.value)}/> 
+      <input placeholder='Password...' onChange = {e => setPassword(e.target.value)}/>
+      <button onClick={signIn}> Sign In </button> 
+      <button onClick={logout}> Sign Out </button> 
+      <button onClick={signInWithGoogle}> Sign In With Google </button> 
     </div>
   )
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,9 @@
 // Import the services you need from firebase then create a variable for it 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from 'firebase/app'; // setup authentication with email/password 
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'; // setup authentication with email/password 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBEtdg_Auln3jC9p5HkkpKq2vS86s01Dsc",
   authDomain: "fir-course-f9728.firebaseapp.com",
@@ -22,3 +18,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app); 
+export const googleProvider = new GoogleAuthProvider(); 
